179f929f0df7f5d1fe5c9f22b2bbb29eaefcc4c6
Yein <flocawint@naver.com>
2021-07-01 20:45:10 +0900

    sp5/chap04로 모듈화

---
+++ sp5/chap04/src/test/java/sp5/chap04/Chap04ApplicationTests.java
@@ -0,0 +1,13 @@
+package sp5.chap04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
