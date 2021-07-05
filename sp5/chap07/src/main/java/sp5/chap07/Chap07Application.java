6479e60c4cf10c50e5a41808600075a408d691c3
Yein <flocawint@naver.com>
2021-07-05 17:37:46 +0900

    JdbcTemplate 기본 설정

---
+++ sp5/chap07/src/main/java/sp5/chap07/Chap07Application.java
@@ -0,0 +1,13 @@
+package sp5.chap07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap07Application.class, args);
+	}
+
+}
---
+++ sp5/chap07/src/test/java/sp5/chap07/Chap07ApplicationTests.java
@@ -0,0 +1,13 @@
+package sp5.chap07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
