6fcd46b6db30877078cca6b5c307b2d6882e2d8e
Yein <flocawint@naver.com>
2021-07-06 23:37:02 +0900

    JdbcTemplate 생성 및 조회 쿼리
    - new JdbcTemplate(DataSource)
    - mapRow(ResultSet, int)

--- sp5/chap08/src/main/java/sp5/chap08/spring/MemberDao.java
+++ sp5/chap08/src/main/java/sp5/chap08/spring/MemberDao.java
@@ -1,16 +1,42 @@
 package sp5.chap08.spring;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MemberDao {
 
+	private JdbcTemplate jdbcTemplate;
+
+	public MemberDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
 	public Member selectByEmail(String email) {
-		return null;
+		List<Member> results = jdbcTemplate.query(
+			"select * from MEMBER where EMAIL = ?",
+			new RowMapper<Member>() {
+				@Override
+				public Member mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+					Member member = new Member(
+						resultSet.getString("EMAIL"),
+						resultSet.getString("PASSWORD"),
+						resultSet.getString("NAME"),
+						resultSet.getTimestamp("REGDATE").toLocalDateTime()
+					);
+					member.setId(resultSet.getLong("ID"));
+					return member;
+				}
+			}, email // Query에서 ?에 들어갈 파라미터
+		);
+		return results.isEmpty() ? null : results.get(0);
 	}
 
 	public void insert(Member member) {
@@ -21,7 +47,17 @@ public class MemberDao {
 
 	}
 
-	public Collection<Member> selectAll() {
-		return null;
+	public List<Member> selectAll() {
+		List<Member> results = jdbcTemplate.query("select * from MEMBER",
+			(ResultSet rs, int rowNum) -> {
+				Member member = new Member(
+					rs.getString("EMAIL"),
+					rs.getString("PASSWORD"),
+					rs.getString("NAME"),
+					rs.getTimestamp("REGDATE").toLocalDateTime());
+				member.setId(rs.getLong("ID"));
+				return member;
+			});
+		return results;
 	}
 }
