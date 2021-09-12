2f87308312f512ec30f051ab5c6bdae426b54a69
Yein <flocawint@naver.com>
2021-09-12 22:13:32 +0900

    feat : Cookie를 활용해 이메일 기억하기

--- sp5/chap10/chap10/src/main/java/controller/LoginController.java
+++ sp5/chap10/chap10/src/main/java/controller/LoginController.java
@@ -1,9 +1,13 @@
 package controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,12 +27,25 @@ public class LoginController {
 	}
 
 	@GetMapping
-	public String form(LoginCommand loginCommand) {
+	public String form(
+		LoginCommand loginCommand,
+		@CookieValue(value = "REMEMBER", required = false) Cookie cookie) {
+
+		// REMEMBER라는 이름의 쿠키가 존재하면, 커맨드 객체의 email 프로퍼티 값 설정
+		if (cookie != null) {
+			loginCommand.setEmail(cookie.getValue());
+			loginCommand.setRememberEmail(true);
+		}
+
 		return "login/loginForm";
 	}
 
 	@PostMapping
-	public String submit(@ModelAttribute("loginCommand") LoginCommand loginCommand, Errors errors, HttpSession httpSession) {
+	public String submit(
+		@ModelAttribute("loginCommand") LoginCommand loginCommand,
+		Errors errors,
+		HttpSession httpSession,
+		HttpServletResponse response) { // 쿠키 생성을 위해
 		new LoginCommandValidator().validate(loginCommand, errors);
 		if (errors.hasErrors()) {
 			return "login/loginForm";
@@ -39,6 +56,16 @@ public class LoginController {
 				loginCommand.getEmail(),
 				loginCommand.getPassword());
 			httpSession.setAttribute("authInfo", authInfo);
+
+			// 쿠키 생성과 삭제
+			Cookie rememberCookie = new Cookie("REMEMBER", loginCommand.getEmail());
+			rememberCookie.setPath("/");
+			if (loginCommand.isRememberEmail()) {
+				rememberCookie.setMaxAge(60 * 60 * 24 * 30);
+			} else {
+				rememberCookie.setMaxAge(0);
+			}
+			response.addCookie(rememberCookie);
 
 			return "login/loginSuccess";
 		} catch (WrongIdPasswordException e) {
--- sp5/chap10/chap10/src/main/webapp/WEB-INF/view/login/loginForm.jsp
+++ sp5/chap10/chap10/src/main/webapp/WEB-INF/view/login/loginForm.jsp
@@ -28,6 +28,11 @@
             <form:errors path="password"/>
         </label>
     </p>
+    <p>
+        <label><spring:message code="rememberEmail"/>
+        <form:checkbox path="rememberEmail"/>
+        </label>
+    </p>
     <input type="submit" value="<spring:message code="login.btn" />">
 </form:form>
 </body>
