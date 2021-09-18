d73225170d48ff92813eb6d4c694556b334a17f6
Yein <flocawint@naver.com>
2021-09-18 22:57:29 +0900

    feat : 잘못된 형식 입력 시 변환 에러 처리

--- sp5/chap10/chap10/src/main/java/controller/MemberListController.java
+++ sp5/chap10/chap10/src/main/java/controller/MemberListController.java
@@ -23,8 +23,13 @@ public class MemberListController {
 	@RequestMapping("/members")
 	public String list(
 		@ModelAttribute("cmd") ListCommand listCommand,
+		Errors errors,
 		Model model
 	) {
+		// @DateTimeFormat에 지정한 형식에 맞지 않으면 Errors 객체에 "typeMismatch" 에러코드 추가
+		if (errors.hasErrors()) {
+			return "member/memberList";
+		}
 		if (listCommand.getFrom() != null && listCommand.getTo() != null) {
 			List<Member> members = memberDao.selectByRegdate(
 				listCommand.getFrom(), listCommand.getTo()
